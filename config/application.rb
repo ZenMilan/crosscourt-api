cb36dd972d5886ffb4d6e08253f5ee85a008cf10
Kevin Pruett <pruett.kevin@gmail.com>
2013-11-25 09:02:10 -0500

    wip -- warden/auth

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,10 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'boot'
 Bundler.require(:default)
 
+Dir[File.expand_path('../initializers/*.rb', __FILE__)].each do |f|
+  require f
+end
+
 Dir[File.expand_path('../../api/*.rb', __FILE__)].each do |f|
   require f
 end
