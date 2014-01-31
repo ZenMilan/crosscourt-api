4dc2a41f4497362df69743fea24e7f013dc9605f
Kevin Pruett <pruett.kevin@gmail.com>
2014-01-31 18:18:36 -0500

    start client associations

--- models/project.rb
+++ models/project.rb
@@ -1,8 +1,11 @@
 class Project < ActiveRecord::Base
   belongs_to :organization
   has_many :features
-  has_many :client_relationships
-  has_many :clients, through: :client_relationships
+
+  # Button this up
+  has_many :relationships
+  has_many :clients, through: :relationships, class_name: "User"
+
   has_one :proposal
 
   validates :name, presence: true, uniqueness: { scope: :organization_id }
--- models/user.rb
+++ models/user.rb
@@ -3,6 +3,10 @@ class User < ActiveRecord::Base
   has_many :affiliations
   has_many :organizations, through: :affiliations
 
+  # Button this up
+  has_many :relationships
+  has_many :projects, through: :relationships
+
   has_secure_password
 
   validates :email, uniqueness: true
