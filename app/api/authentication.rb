9c5d5d7bc6e518b99c45cb7fc76ad994475aa3fd
Kevin Pruett <pruett.kevin@gmail.com>
2014-08-15 16:55:13 -0400

    wip

--- app/api/authentication.rb
+++ app/api/authentication.rb
@@ -14,24 +14,6 @@ module Crosscourt
           redirect "#{ENV['REGISTRATION_REDIRECT']}#{URI.escape(uri_params)}"
         end
       end
-
-      # get '/set_cookie' do
-      #   cookies[:foo] = {
-      #     value: 'bar',
-      #     domain: ENV['COOKIE_DOMAIN']
-      #   }
-      #
-      #   env['rack.session'][:foo] = 'bar'
-      #
-      #   binding.pry
-      #   { message: 'set cookie foo to bar' }
-      # end
-      #
-      # get '/check_cookie' do
-      #   env['rack.session'][:init] = true
-      #   binding.pry
-      #   { message: 'check yo cookies' }
-      # end
     end
   end
 end
